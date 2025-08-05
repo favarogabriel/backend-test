a19507591c31cb6b47a82f36b62a2596929f5afb
Gabriel Favaro <gabrielffav@gmail.com>
2025-08-04 23:43:03 -0300

    wip

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.get('/verify-code', (req, res) => {
     }
 })
 
-app.post('/submit', (req, res) => {
+app.get('/submit', (req, res) => {
     fs.writeFile('data.json', JSON.stringify(req.query), (err) => {
         if (err) {
             res.status(500).send('Error saving data');
