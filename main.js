d2fe59a906661e3b120303f6d51dff3f3be93eae
Gabriel Favaro <gabrielffav@gmail.com>
2025-08-04 23:16:22 -0300

    wip

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
 });
