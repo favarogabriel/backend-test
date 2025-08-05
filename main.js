dc174e2d54241ba91268786597a21c0a2d20b90d
Gabriel Favaro <gabrielffav@gmail.com>
2025-08-04 23:32:33 -0300

    wip

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.get('/verify-code', (req, res) => {
+    if (req.query.code === '1234') {
+        res.send('Code verified successfully');
+    } else {
+        res.status(400).send('Invalid code');
+    }
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on ${process.env.URL}:${process.env.PORT}`);
 });
