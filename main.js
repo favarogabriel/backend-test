c5fcd3a7c82611bcbfa0c3b2d28177c3e32672ea
Gabriel Favaro <gabrielffav@gmail.com>
2025-08-04 23:25:58 -0300

    wip

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on ${process.env.URL}:${process.env.PORT || 3000}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.URL}:${process.env.PORT}`);
 });
